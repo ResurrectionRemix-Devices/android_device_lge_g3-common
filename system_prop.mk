da3ed6120c0dd3e877550d520e7fb5a94b2f09dc
westcripp <korukaltan@gmail.com>
2015-04-20 19:52:45 +0300

    Tweak up
    Change-Id: I0324fe0dff24fe870b4146ad26693f8059b31833

--- system_prop.mk
+++ system_prop.mk
@@ -29,7 +29,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.mdpcomp.enable=true \
     ro.opengles.version=196608 \
-    ro.sf.lcd_density=560
+    ro.sf.lcd_density=534
 
 # GPS
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -61,7 +61,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.add_power_save=1 \
     persist.radio.apm_sim_not_pwdn=1 \
-    ro.telephony.ril_class=LgeLteRIL \
+    ro.telephony.ril_class=LgeLteRIL
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.netmgrd.qos.enable=false \
